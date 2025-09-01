49858036f30fa13684f53920b5a51584a3872ed4
gmarinw <gisel.marin.caro@gmail.com>
2025-08-31 22:34:03 -0600

    Fibonacci

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -227,5 +227,24 @@ namespace ConsoleApp1
             } while (k <= n2);
         }
 
+        void Fibonacci()
+        {
+            int limit = 15;
+            int n1 = 0;
+            int n2 = 1;
+            Console.WriteLine(n1);//imprimo n1
+            Console.WriteLine(n2);//imprimo n2
+
+            int res = n1 + n2; //1
+
+            while (res < limit)
+            {
+                Console.WriteLine(res);//imprimo la suma
+                n1 = n2;
+                n2 = res;
+                res = n1 + n2; //
+            }
+        }
+
     }
 }
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿metodo();
+static void metodo()
+{
+    int x = 12321;
+    string str = x.ToString();
+    for (int i = 0; i < (str.Count() / 2); i++)
+    {
+        int n = str[str.Count() - 1 - i];
+        if (str[i] != str[str.Count() - 1 - i])
+        {
+            Console.Write("No es palindromo");
+        }
+    }
+    Console.Write("Es palindromo");
+}
